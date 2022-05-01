8b0551f81027aa0c2c701429218d06f952249811
gilivex <74468196+gilivex@users.noreply.github.com>
2022-05-01 17:59:49 +0300

    last update

--- Test.cpp
+++ Test.cpp
@@ -14,35 +14,100 @@
 using namespace std;
 using namespace coup;
 
-Game game1{};
-Game game2{};
-Ambassador ambassador1(game1, "amb1");
-Ambassador ambassador2(game2, "amb2");
-Assassin assassin1(game1, "ass1");
-Assassin assassin2(game1, "ass2");
-Captain captain1(game1, "cap1");
-Captain captain2(game2, "cap2");
-Contessa contessa1(game1, "con1");
-Contessa contessa2(game2, "con2");
-Duke duke1(game1, "duk1");
-Duke duke2(game2, "duk2");
+Game game_1{};
+Game game_2{};
+Ambassador ambassador1(game_1, "amb1");
+Ambassador ambassador2(game_2, "amb2");
+Assassin assassin1(game_1, "ass1");
+Assassin assassin2(game_2, "ass2");
+Captain captain1(game_1, "cap1");
+Captain captain2(game_2, "cap2");
+Contessa contessa1(game_1, "con1");
+Contessa contessa2(game_2, "con2");
+Duke duke1(game_1, "duk1");
+Duke duke2(game_2, "duk2");
 
 
 
 TEST_CASE("Game is alive")
 {
-    vector<string> players = {"amb1", "ass1", "cap1", "con1", "duke1", "amb2"};
+    vector<string> players = {"amb1", "ass1","duk1","con1", "cap1", "amb2"};
     for (uint i = 0; i < players.size(); i++)
     {
-        CHECK_EQ(game1.turn(), players.at(i));
+        CHECK_EQ(game_1.turn(), players.at(i));
     }
 }
 
-TEST_CASE("bad turns"){
-    ambassador1.income();
-    CHECK_THROWS(captain1.income());
-    CHECK_THROWS(game1.winner());
-    for (int i = 0; i <20 ; ++i) {
-        CHECK(1==1);
+TEST_CASE("Game is alive")
+{
+    vector<string> players = {"amb2", "ass2","duk2","con2", "cap2", "amb1"};
+    for (uint i = 0; i < players.size(); i++)
+    {
+                CHECK_EQ(game_2.turn(), players.at(i));
     }
+}
+TEST_CASE("bad turns") {
+
+            CHECK_EQ(game_1.turn(), "amb1");
+    ambassador1.income();
+            CHECK_EQ(game_1.turn(), "ass1");
+    assassin1.income();
+            CHECK_EQ(game_1.turn(), "duk1");
+    duke1.income();
+            CHECK_EQ(game_1.turn(), "con1");
+    contessa1.income();
+            CHECK_EQ(game_1.turn(), "cap1");
+    captain1.income();
+            CHECK_EQ(game_1.turn(), "amb1");
+    ambassador1.income();
+
+    CHECK_THROWS(duke1.income());
+
+            CHECK_EQ(game_2.turn(), "amb1");
+    ambassador2.income();
+            CHECK_EQ(game_2.turn(), "ass1");
+    assassin2.income();
+            CHECK_EQ(game_2.turn(), "duk1");
+    duke2.income();
+            CHECK_EQ(game_2.turn(), "con1");
+    contessa2.income();
+            CHECK_EQ(game_2.turn(), "cap1");
+    captain2.income();
+            CHECK_EQ(game_2.turn(), "amb1");
+    ambassador1.income();
+
+    CHECK_THROWS(contessa2.income());
+}
+
+TEST_CASE("good turns") {
+
+            CHECK_EQ(game_1.turn(), "amb1");
+    ambassador1.income();
+            CHECK_EQ(game_1.turn(), "ass1");
+    assassin1.income();
+            CHECK_EQ(game_1.turn(), "duk1");
+    duke1.income();
+            CHECK_EQ(game_1.turn(), "con1");
+    contessa1.income();
+            CHECK_EQ(game_1.turn(), "cap1");
+    captain1.income();
+            CHECK_EQ(game_1.turn(), "amb1");
+    ambassador2.income();
+
+    CHECK_NOTHROW(ambassador2.income());
+
+            CHECK_EQ(game_2.turn(), "amb1");
+    ambassador2.income();
+            CHECK_EQ(game_2.turn(), "ass1");
+    assassin2.income();
+            CHECK_EQ(game_2.turn(), "duk1");
+    duke2.income();
+            CHECK_EQ(game_2.turn(), "con1");
+    contessa2.income();
+            CHECK_EQ(game_2.turn(), "cap1");
+    captain2.income();
+            CHECK_EQ(game_2.turn(), "amb1");
+    ambassador1.income();
+
+    CHECK_NOTHROW(ambassador1.income());;
 }
